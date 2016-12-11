0189c1240b9b766074757a772ec0bc6cf3d26c19
schnable <atschnable@gmail.com>
2016-12-11 10:12:44 -0500

    Added new tiggers and logging

--- mycode.php
+++ mycode.php
@@ -17,9 +17,20 @@ class mycode
     public static function init()
     {
         // error_log("mycode::init");
+        add_filter('woocommerce_login_redirect',    array(__CLASS__,'login_redirect'));
 
-        add_action('woocommerce_payment_complete', array(__CLASS__,'process_order'), 10, 1);
-        add_filter('woocommerce_login_redirect',   array(__CLASS__,'login_redirect'));
+        // For now, log all of these so we can work out the workflow triggers
+
+        add_action('woocommerce_payment_complete',        array(__CLASS__,'process_order'));
+
+        add_action('woocommerce_order_status_cancelled',  array(__CLASS__,'order_status'));
+        add_action('woocommerce_order_status_refunded',   array(__CLASS__,'order_status'));
+        add_action('woocommerce_order_status_on-hold',     array(__CLASS__,'order_status'));
+        add_action('woocommerce_order_status_processing', array(__CLASS__,'order_status'));
+        add_action('woocommerce_order_status_failed',     array(__CLASS__,'order_status'));
+        add_action('woocommerce_order_status_pending',    array(__CLASS__,'order_status'));
+        add_action('woocommerce_order_status_completed',  array(__CLASS__,'order_status'));
+        add_action('woocommerce_checkout_order_processed',array(__CLASS__,'order_status'));
     }
 
     public static function activate()
@@ -44,29 +55,38 @@ class mycode
         return $redirect_to;
     }
 
+    function _log_order($order_id)
+    {
+        $order = new WC_Order( $order_id );
+        $status = $order->status;
+
+        $user_id = (int)$order->user_id;
+        $user_info = get_userdata($user_id);
+        $items = $order->get_items();
+
+        error_log("mycode::_log_order: status: $status, user: $user_id, item count: " . count($items));
+
+        // foreach ($items as $item) {
+            // error_log("mycode::_log_order: item: " .  json_encode($item));
+        // }
+    }
+
+    function order_status($order_id)
+    {
+        error_log("mycode::order_status");
+        self::_log_order($order_id);
+        return $order_id;
+    }
+
     function process_order($order_id)
     {
         error_log("mycode::process_order");
-
-        $order = new WC_Order( $order_id );
-        $myuser_id = (int)$order->user_id;
-        $user_info = get_userdata($myuser_id);
-        $items = $order->get_items();
-
-        foreach ($items as $item) {
-
-            error_log("process_order: " .  json_encode($item));
-
-            if ($item['product_id']==24) {
-              // Do something clever
-            }
-        }
+        self::_log_order($order_id);
         return $order_id;
     }
 }
 
-add_action('plugins_loaded', array('mycode', 'init'));
-
-register_activation_hook(__FILE__, array('mycode', 'activate'));
+add_action('plugins_loaded',         array('mycode', 'init'));
+register_activation_hook(__FILE__,   array('mycode', 'activate'));
 register_deactivation_hook(__FILE__, array('mycode', 'deactivate'));
-register_uninstall_hook(__FILE__, array('mycode', 'uninstall'));
+register_uninstall_hook(__FILE__,    array('mycode', 'uninstall'));
